adc403e3c8a2daea8d2c2695926c353ef84098c4
Gavin <tianyao@zju.edu.cn>
2019-06-17 03:30:00 +0800

    linked table 2164

--- ZOJ/2164/main.cpp
+++ ZOJ/2164/main.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 #define MAX_NUM 55
+
 struct pokerList{
     int num;
     pokerList* next;
+    pokerList* pre;
 };
 
 int list[MAX_NUM];
@@ -12,23 +14,65 @@ int list[MAX_NUM];
 int n,r;
 
 int main() {
-    int p,c;
+    int pp,cc;
     cin>>n>>r;
     while (n!=0 || r!=0){
-        pokerList* HeadNode = new pokerList;
-        pokerList* p = HeadNode;
-        for (int i=1;i<n;i++){
-            p->num = i;
-            p->next = new pokerList;
-            p = p->next;
-        }
-        
-        for (int i=0;i<n;i++){
-            cin>>p>>c;
+        pokerList* Head = new pokerList;
+        pokerList* p = Head;
+        pokerList* q = Head;
 
+        for (int i=1;i<=n;i++){
+            p->num = i;
+            p->next = i==n ? NULL : new pokerList;
+            p->pre = i==1 ? NULL : q;
+
+            p = p->next;
+            q = i==1 ? q : q->next;
+        }
+
+        for (int i = 1;i <= r;i++){
+            cin>>pp>>cc;
+            pokerList* tail = Head;
+            pokerList* b;
+            pokerList* t;
+
+            while(tail->next != NULL)
+                tail = tail->next;
+
+            b = tail;
+            for (int j = 1;j <= pp - 1; j++)
+                b = b->pre;//last bottom
+            if (b == tail) continue;
+
+            t = b;
+            for (int j = 1;j<= cc - 1;j++)
+                t = t->pre;//last cut
+
+            if (t->pre == NULL){
+                tail->next = t;
+                t->pre = tail;
+
+                b->next->pre = NULL;
+                b->next = NULL;
+            }
+            else{
+                tail->next = t;
+                t->pre->next = b->next;
+                b->next->pre = t->pre;
+                t->pre = tail;
+                b->next = NULL;
+            }
+            while (Head->pre!=NULL) Head = Head->pre;
+        }
+        while (Head->next!=NULL) Head = Head->next;
+        cout<<Head->num<<endl;
+
+        while (Head->pre!=NULL){
+            p = Head;
+            Head = Head->pre;
+            delete p;
         }
         cin>>n>>r;
     }
-
     return 0;
 }
